c8c286855327479e42a9dcdf2a5703e6160b0535
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-03-09 21:51:52 +0100

    Izmenjene klase Autor i Knjiga

--- Biblioteka/src/Autor/Autori.java
+++ Biblioteka/src/Autor/Autori.java
@@ -1,5 +1,14 @@
 package Autor;
 
 public class Autori {
-
+@Override
+public String toString() {
+	// TODO Auto-generated method stub
+	return super.toString();
+}
+@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 }
--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -10,5 +10,45 @@ LinkedList<Autori> autori;
 String izdavac;
 int izdanje;
 String isbn;
+public String getNaslov() {
+	return naslov;
+}
+public void setNaslov(String naslov) {
+	this.naslov = naslov;
+}
+public LinkedList<Autori> getAutori() {
+	return autori;
+}
+public void setAutori(LinkedList<Autori> autori) {
+	this.autori = autori;
+}
+public String getIzdavac() {
+	return izdavac;
+}
+public void setIzdavac(String izdavac) {
+	this.izdavac = izdavac;
+}
+public int getIzdanje() {
+	return izdanje;
+}
+public void setIzdanje(int izdanje) {
+	this.izdanje = izdanje;
+}
+public String getIsbn() {
+	return isbn;
+}
+public void setIsbn(String isbn) {
+	this.isbn = isbn;
+}
+@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 
 }
