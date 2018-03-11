47fb63b7f9f175e56363c554b121630405714ae5
JovanaDrobnjakovic <drobnjakovic2216@fon.bg.ac.rs>
2018-03-11 20:23:56 +0100

    napravljena je klasa Knjiga

---
+++ Biblioteka/src/biblioteka/biblioteka.java
@@ -0,0 +1,34 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import Autor.Autori;
+import interfejs.bibliotekaInterfejs;
+
+public class biblioteka implements bibliotekaInterfejs {
+	LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+
+
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+
+	}
+
+	
+	public LinkedList<Knjiga> VratiSveKnjige() {
+		return knjige;
+	}
+
+	
+	public LinkedList<Knjiga> pronadjiKnjigu(Autori autor, long ISBN, String naslov, String izdavac) {
+	return knjige;	
+		
+	}
+
+}
